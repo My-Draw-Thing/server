6e87a4ce1193c083ac4d85f78c8e333dd4734b8d
candra saputra <candrasaputra@live.com>
2019-09-19 20:34:17 +0700

    name validation

--- models/user.js
+++ models/user.js
@@ -5,7 +5,29 @@ const { bcrypt } = require('../helpers')
 const userSchema = new Schema({
     name: {
         type: String,
-        required: [true, 'Name is required']
+        required: [true, 'Name is required'],
+        validate: [
+            {
+                validator: function (val) {
+                    return mongoose.model('Users', userSchema).find({
+                        _id: {
+                            $ne: this._id
+                        },
+                        name: val
+                    })
+                        .then(data => {
+                            if (data.length !== 0) {
+                                return false
+                            }
+                        })
+                        .catch(err => {
+                            return err.message
+                        })
+                },
+                message: 'Name has been used'
+            }
+        ],
+        minlength: [5, 'Name must to have 5 characters as minimum']
     },
     status: {
         type: Boolean,
